0b52f0b13fbd5653fd055725b4b19c8536de4b78
mrsho <paul.sholter@edu.sait.ca>
2023-04-17 08:40:08 -0600

    create py file name

--- paul_patients_patientmanager.py
+++ paul_patients_patientmanager.py
@@ -1,9 +1,14 @@
 class Patient:
-    def __int__(self, id, name, disease , gender, age):
+    def __init__(self, id, name, disease , gender, age):
         self.id = id
         self.name = name
         self.disease = disease
         self.gender = gender
         self.age = age
 
+
+patient1 = Patient(12, 'Pankaj', 'Cancer', 'Male', 30)
+patient2 = Patient(13, 'Sumit', 'Cold', 'Male', 23)
+patient3 = Patient(14, 'Alok', 'Maleriya', 'Male', 45)
+patient4 = Patient(15, 'Ravi', 'Diabetes', 'Male', 25)
 
