b8ffcb41ab56a02003956ed8d78b15115b8eb4a0
mrsho <paul.sholter@edu.sait.ca>
2023-04-16 21:20:17 -0600

    create py file name

---
+++ paul_patients_patientmanager.py
@@ -0,0 +1,9 @@
+class Patient:
+    def __int__(self, id, name, disease , gender, age):
+        self.id = id
+        self.name = name
+        self.disease = disease
+        self.gender = gender
+        self.age = age
+
+
