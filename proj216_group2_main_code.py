254ba915eb13ac1b115f1b4c47efb2cb24f42686
MatthewAMK <matthewamkoncz@gmail.com>
2023-04-23 13:38:28 -0600

    Update

--- proj216_group2_main_code.py
+++ proj216_group2_main_code.py
@@ -179,9 +179,13 @@ class Patient:
     def get_age(self):
         return self.age
 
+    def set_name(self, new_patient_name):
+        self.patient_name = new_patient_name
+
     def set_patient_id(self, new_patient_id):
         self.patient_id = new_patient_id
-        self.patient_name = new_patient_name
+    def set_patient_id(self, new_patient_id):
+        self.patient_id = new_patient_id
 
     def set_disease(self, new_disease):
         self.disease = new_disease
@@ -288,5 +292,76 @@ class PatientManagement:
             print(f"Patients whose ID is {new_patient.get_patient_id()} has been added")
 
 
+class Management:
+    @staticmethod
+    def display_menu():
+        main_menu = 'Welcome to Alberta Hospital (AH) Management System\n' \
+                    'Select from the following options, or select 3 to stop:\n' \
+                    '1 -  Doctors\n' \
+                    '2 -  Patients\n' \
+                    '3 -  Exit Program\n'
+
+        choice = ''
+        while choice != '3':
+            choice = input(main_menu)
+            if choice == '1':
+                Management.doctor_menu()
+            elif choice == '2':
+                Management.patient_menu()
+
+        print('Thanks for using the program. Bye!')
+
+    @staticmethod
+    def doctor_menu():
+
+        doctor_manager = DoctorManager()
+
+        doctor_menu = 'Doctors Menu:\n' \
+                      '1 - Display Doctors list\n' \
+                      '2 - Search for doctor by ID\n' \
+                      '3 - Search for doctor by name\n' \
+                      '4 - Add doctor\n' \
+                      '5 - Edit doctor info\n' \
+                      '6 - Back to the Main Menu\n'
+
+        option = ''
+        while option != '6':
+            option = input(doctor_menu)
+            if option == '1':
+                print(doctor_manager.display_doctors_list())
+            elif option == '2':
+                doctor_manager.search_doctor_by_id()
+            elif option == '3':
+                doctor_manager.search_doctor_by_name()
+            elif option == '4':
+                doctor_manager.add_dr_to_file()
+            elif option == '5':
+                doctor_manager.edit_doctor_info()
+
+    @staticmethod
+    def patient_menu():
+
+        patient_manager = PatientManagement()
+
+        patient_menu = 'Patients Menu:\n' \
+                       '1 - Display patients list\n' \
+                       '2 - Search for patient by ID\n' \
+                       '3 - Add patient\n' \
+                       '4 - Edit patients info\n' \
+                       '5 - Back to the Main Menu\n'
+
+        option = ''
+        while option != '5':
+            option = input(patient_menu)
+            if option == '1':
+                patient_manager.display_patient_list()
+            elif option == '2':
+                patient_manager.search_patient_by_id()
+            elif option == '3':
+                patient_manager.enter_patient_info()
+            elif option == '4':
+                patient_manager.edit_patient_info()
 
 
+manager = Management()
+manager.display_menu()
